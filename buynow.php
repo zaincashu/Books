2dbecd20559d08c8e0bdb72d8c4f6d08d8a01261
zaincashu <34888458+zaincashu@users.noreply.github.com>
2018-08-15 09:12:37 +0300

    Update buynow.php

--- buynow.php
+++ buynow.php
@@ -3,19 +3,16 @@
 <head>
 <title>Apple's iTunes $25 Card</title>
 </head>
-</body>
+<body>
 
 
 <form action='https://www.2checkout.com/checkout/purchase' method='post'>
   <input type='hidden' name='sid' value='203831924'>
   <input type='hidden' name='quantity' value='1'>
   <input type='hidden' name='product_id' value='1'>
-  <label>Quantity</label>
-  <input name='quantity' type='text' size='5'>
-  <input name='submit' type='submit' value='Buy from 2CO'>
+  <input name='submit' type='submit' value='Buy from 2CO' >
 </form>
 <span>2Checkout.com Inc. (Ohio, USA) is a payment facilitator for goods and services provided by eBooks&eGift-card.</span>
-                    
 
 
 </body>
